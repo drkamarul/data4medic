68dee5c98dd9d397db182df164dd2e55037ca354
drkamarul <drki.musa@gmail.com>
2021-01-02 13:46:21 +0800

    added files

---
+++ R/metabolic.R
@@ -0,0 +1,12 @@
+#' metabolic syndrome dataset
+#' Subjects of a metabolic syndrome study
+#' @format A dataframe with columns and rows
+#' \describe{
+#'  \item{ID}{ID}
+#'  \item{RURAL}{Rural=1 or Urban=2}
+#'  \item{DOB}{Date of Birth}
+#'  \item{AGE}{Age of subjects}
+#'  \item{DMDX}{History of Diabetes, 0=No, 1=Yes}
+#'  \item{DISLIPID}{History of Dyslipidemia, 0=No, 1=Yes}
+#' }
+"metabolic"
